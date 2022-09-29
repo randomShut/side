d9613323cd02417535a9ac93c63e14b040ebe426
randomShut <112585668+randomShut@users.noreply.github.com>
2022-09-29 02:37:04 +0200

    Update script.js

--- kalender/script.js
+++ kalender/script.js
@@ -7,7 +7,7 @@ const newEventModal = document.getElementById('newEventModal');
 const deleteEventModal = document.getElementById('deleteEventModal');
 const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const weekdays = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
 
 function openModal(date) {
   clicked = date;
